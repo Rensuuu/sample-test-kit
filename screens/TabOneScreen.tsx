4d33f44eae06dd79a77c2c38606aca4e8ef8316b
Rensuuu <rencedetorres@gmail.com>
2022-03-19 15:10:19 +0800

    done activity

--- screens/TabOneScreen.tsx
+++ screens/TabOneScreen.tsx
@@ -1,45 +1,237 @@
-import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import Footer from '../components/Folder/Footer';
-import Header from '../components/Folder/Header';
-import Login from '../components/Folder/Login';
+import React, { useState } from 'react';
+import { View, StyleSheet, Text, TouchableOpacity, Dimensions, Alert } from 'react-native';
+import { TextInput } from 'react-native-paper';
+import LottieView from 'lottie-react-native';
 
-export default function TabOneScreen() {
+// REGISTRATION-SCREEN
+
+export default function TabTwoScreen() {
+const [name, setName] = useState<string>()
+const [surname, setSurname] = useState<string>()
+const [username, setUsername] = useState<string>()
+const [email, setEmail] = useState<string>()
+const [password, setPassword] = useState<string>()
+const [cpassword, setCpassword] = useState<string>()
+
+const ErrorReg = () => {
+    const nulltxt = ""
+    if (name === nulltxt || surname === nulltxt || email === nulltxt || password === nulltxt || cpassword === nulltxt || username === nulltxt) {
+        return Alert.alert("Message", "Please input all necessary data");
+      }
+       Alert.alert("Message", "Successfully Registered!");
+}
 return (
-  // HEADER
-  <View style={styles.container}>
+
+
+
+<View style={styles.container}>
+    {/* HEADER */}
     <View style={styles.head}>
-      <Header />
+         <LottieView style={styles.lottieHead}
+            source={
+              require('../assets/lottie/68766-register.json')
+            }
+            autoPlay
+            />
     </View>
-    <View style={styles.log}>
-      <Login />
+
+    {/* REGISTER */}
+    <View style={styles.register}>
+
+        <Text style={{
+            fontFamily:'poppins-semi-bold',
+            fontSize: 40,
+            alignItems:'center'
+        }}>
+            Registration Screen
+        </Text>
+        <View style={styles.txtContainer}>
+            <TextInput 
+            label='Please type your name'
+            style={styles.regContainer}
+            autoComplete={false} 
+            mode={'outlined'}
+            onChangeText={(value: string) => {
+            setName(value);
+            }}
+            value={name}
+            />
+        </View>
+        <View style={styles.txtContainer}>
+            <TextInput 
+            label='Please type your surname'
+            style={styles.regContainer}
+            autoComplete={false} 
+            mode={'outlined'}
+            onChangeText={(value: string) => {
+            setSurname(value);
+            }}
+            value={surname}
+            />
+        </View>
+        <View style={styles.txtContainer}>
+            <TextInput 
+            label='Type e-mail address here'
+            style={styles.regContainer}
+            autoComplete={false} 
+            mode={'outlined'}
+            onChangeText={(value: string) => {
+            setEmail(value);
+            }}
+            value={email}
+            />
+        </View>
+        <View style={styles.txtContainer}>
+            <TextInput 
+            label='Type your username'
+            style={styles.regContainer}
+            autoComplete={false} 
+            mode={'outlined'}
+            onChangeText={(value: string) => {
+            setUsername(value);
+            }}
+            value={username}
+            />
+        </View>
+        <View style={styles.txtContainer}>
+            <TextInput 
+            label='Type your password'
+            style={styles.regContainer} 
+            autoComplete={false}
+            mode={'outlined'}
+            onChangeText={(value: string) => {
+            setPassword(value);
+            }}
+            value={password}
+            secureTextEntry
+            />
+        </View>
+        <View style={styles.txtContainer}>
+            <TextInput 
+            label='Confirm password'
+            style={styles.regContainer}
+            autoComplete={false} 
+            mode={'outlined'}
+            onChangeText={(value: string) => {
+            setCpassword(value);
+            }}
+            value={cpassword}
+            secureTextEntry
+            />
+        </View>
+
+        <View style={styles.toContainer}>
+            <TouchableOpacity onPress={ErrorReg}>
+                <Text style={{
+                    fontSize:25,
+                    fontFamily:'poppins-semi-bold'
+                }}>
+                    Register
+                </Text>
+            </TouchableOpacity>
+        </View>
     </View>
+
+     {/* FOOTER */}
     <View style={styles.foot}>
-    <Footer />
+        <Text style={styles.footerTxt}>App Dev</Text>
     </View>
-  </View>
-  
-  
-  
-  )
+</View>
+)
 }
 const styles = StyleSheet.create({
-  head: {
-   flex: 2,
+    container:{
+    height: Dimensions.get('screen').height,
+    backgroundColor:'wheat',
+    },
+    regContainer:{
+    color:'black',
+    fontSize:16,
+    fontFamily:'poppins-regular',
+    height: 50,
+    },
+    head:{
+        flex:1,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    register:{
+        flex:3,
+        justifyContent:'center',
+        alignItems:'center',
+    },
+    foot:{
+        flex:.5,
+        justifyContent:'flex-end',
+        alignItems:'center',
+    },
+    txtContainer:{
+        height:50,
+        fontFamily:'poppins-regular',
+        width:280,
+        marginTop:20,
+    },
+    toContainer:{
+    color:'black',
+    marginTop:40,
+    height:48,
+    width: 120,
+    backgroundColor:'#F9EDEB',
     justifyContent:'center',
-    alignItems:'center'
-  },
-  container: {
-    flex: 1,
-    backgroundColor: 'wheat'
-  },
-  log:{
-    flex: 2,
-  },
-foot:{
-  flex:1,
-  justifyContent:'flex-end',
-  alignItems:'center',
-  paddingBottom:15,
-},
+    alignItems:'center',
+    borderRadius:15,
+    borderColor:'black',
+    borderWidth:1
+    },
+    lottieHead:{
+        marginTop:15,
+    },
+    footerTxt:{
+    fontFamily:'poppins-bold', 
+    fontSize:14,
+    paddingBottom:7
+    }
 })
+
+// TABONE-ORIG
+// import React from 'react';
+// import { View, StyleSheet } from 'react-native';
+// import Footer from '../components/Folder/Footer';
+// import Header from '../components/Folder/Header';
+// import Login from '../components/Folder/Login';
+// export default function TabTwoScreen() {
+// return (
+//   // HEADER
+//   <View style={styles.container}>
+//     <View style={styles.head}>
+//       <Header />
+//     </View>
+//     <View style={styles.log}>
+//       <Login />
+//     </View>
+//     <View style={styles.foot}>
+//     <Footer />
+//     </View>
+//   </View>
+//   )
+// }
+// const styles = StyleSheet.create({
+//   head: {
+//    flex: 2,
+//     justifyContent:'center',
+//     alignItems:'center'
+//   },
+//   container: {
+//     flex: 1,
+//     backgroundColor: 'wheat'
+//   },
+//   log:{
+//     flex: 2,
+//   },
+// foot:{
+//   flex:1,
+//   justifyContent:'flex-end',
+//   alignItems:'center',
+//   paddingBottom:15,
+// },
+// })
--- screens/TabTwoScreen.tsx
+++ screens/TabTwoScreen.tsx
@@ -1,31 +1,83 @@
-import { StyleSheet } from 'react-native';
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+import Footer from '../components/Folder/Footer';
+import Header from '../components/Folder/Header';
+import Login from '../components/Folder/Login';
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-
-export default function TabTwoScreen() {
-  return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Tab Two</Text>
-      <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-      <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
-    </View>
-  );
+{// TABTWOSCREEN
+// import { StyleSheet } from 'react-native';
+// import EditScreenInfo from '../components/EditScreenInfo';
+// import { Text, View } from '../components/Themed';
 }
 
+// LOG-IN
+
+export default function TabOneScreen() {
+  return (
+   // HEADER
+    <View style={styles.container}>
+
+    <View style={styles.head}>
+    <Header />
+    </View>
+
+    <View style={styles.log}>
+    <Login />
+    </View>
+
+    <View style={styles.foot}>
+    <Footer />
+    </View>
+
+    </View>
+
+
+    
+    // TABTWOSCREEN-INSIDE
+  );
+}
+    // TABTWOSCREEN-INSIDE
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Tab Two</Text>
+    //   <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
+    //   <EditScreenInfo path="/screens/TabTwoScreen.tsx" />
+    // </View>
+
 const styles = StyleSheet.create({
+  head: {
+   flex: 2,
+    justifyContent:'center',
+    alignItems:'center'
+  },
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'wheat'
   },
-  title: {
-    fontSize: 20,
-    fontWeight: 'bold',
+  log:{
+    flex: 2,
   },
-  separator: {
-    marginVertical: 30,
-    height: 1,
-    width: '80%',
-  },
-});
+foot:{
+  flex:1,
+  justifyContent:'flex-end',
+  alignItems:'center',
+  paddingBottom:15,
+},
+})
+
+// TABTWOSCREEN
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   title: {
+//     fontSize: 20,
+//     fontWeight: 'bold',
+//   },
+//   separator: {
+//     marginVertical: 30,
+//     height: 1,
+//     width: '80%',
+//   },
+// });
